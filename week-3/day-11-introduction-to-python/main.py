a302f9b6af3955cea69d17b0fb51d792ae1e060d
Essell Chigozie <essellchigozie@gmail.com>
2026-09-22 23:12:19 -0700

    feat(day-11): complete introduction to Python exercises

--- week-3/day-11-introduction-to-python/main.py
+++ week-3/day-11-introduction-to-python/main.py
@@ -7,13 +7,35 @@
 # Print all of them with descriptive labels.
 
 # TODO: your code here
+Name = "Chigozie"
+Age = 20
+Is_an_engineer = True
+Rating = 4.9
 
+print(Name)
+print(Age)
+print(Is_an_engineer)
+print(Rating)
+
+print(type(Name))
+print(type(Age))
+print(type(Is_an_engineer))
+print(type(Rating))
 
 # ── Exercise 2: Temperature Converter ────────────────────────────────────────
 # Ask the user to enter a temperature in Celsius, then print the Fahrenheit equivalent.
 # Also convert in the opposite direction (Fahrenheit to Celsius).
 
 # TODO: your code here
+# celsius to fahrenheit
+Temp = float(input("Enter temperature in celsius: "))
+conversion = (Temp * 9/5) + 32
+print(Temp, "degree celsuis converted to", conversion,"degree fahrenheit")
+
+# fahrenheit to celsuis
+Temp = float(input("Enter temperature in fahrenheit: "))
+conversion = (Temp - 32) * 5/9
+print(Temp, "degree fahrenheit converted to", conversion,"degree celsius")
 
 
 # ── Exercise 3: Age Calculator ────────────────────────────────────────────────
@@ -21,3 +43,7 @@
 # Calculate and print their current age and the year they will turn 30.
 
 # TODO: your code here
+User_Name = input("Enter your name> ")
+Birth_year = int(input("Enter your birth year> "))
+future_year = Birth_year + 30
+print("You will be 30 years old by", future_year)
